606af66ecca9c0bb5361bdd320a7e9025e42a111
Sri Prasanna <k.sriprasanna@gmail.com>
2013-01-24 10:43:46 +0300

    +Nishant - removing django unit test for views

--- fred_consumer/tests/unit/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.test.client import Client
-
-class TestViews(TestCase):
-
-  def test_fred_config_page(self):
-    client = Client()
-    response = client.get('/fredconsumer/')
-    self.assertEqual(response.status_code, 200)
