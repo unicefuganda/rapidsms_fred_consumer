765368f11b07b51a9ef4fa7f2923dd70dd3e8e71
Bharti <bhartin@thoughtworks.com>
2013-01-25 14:29:51 +0300

    renaming model file

--- fred_consumer/migrations/0003_add_model_HealthFacilityIdMap.py
+++ fred_consumer/migrations/0003_add_model_HealthFacilityIdMap.py
@@ -25,14 +25,20 @@ class Migration(SchemaMigration):
         'fred_consumer.fredconfig': {
             'Meta': {'object_name': 'FredConfig'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
-            'username': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
         'fred_consumer.healthfacilityidmap': {
             'Meta': {'object_name': 'HealthFacilityIdMap'},
             'uid': ('django.db.models.fields.CharField', [], {'max_length': '50', 'primary_key': 'True'}),
             'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        'fred_consumer.jobstatus': {
+            'Meta': {'object_name': 'JobStatus'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job_id': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
         }
     }
 
