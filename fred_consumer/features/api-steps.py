6f855c3fbbdb11e36b45e50a8f0a52c25c8acb1b
Sri Prasanna <k.sriprasanna@gmail.com>
2013-03-12 12:51:06 +0300

    Fixing api spec

--- fred_consumer/features/api-steps.py
+++ fred_consumer/features/api-steps.py
@@ -1,5 +1,5 @@
 from lettuce import *
-from fred_consumer.fred_connect import * 
+from fred_consumer.fred_connect import *
 from urllib2 import HTTPError
 import random
 import string
@@ -12,39 +12,39 @@ def new_name():
 
 @step(u'Given I have created a facility')
 def given_i_have_created_a_facility(step):
-  world.name = "Facility " + new_name() 
-  connector.write(credentials['url'], {
-    'name'       : world.name, 
+  world.name = "Facility " + new_name()
+  connector.write(credentials['url'] + "/facilities", {
+    'name'       : world.name,
     'active'     : True,
     'coordinates': [0,0]})
 
 @step(u'Then it should appear at the top of the feed')
 def then_it_should_appear_at_the_top_of_the_feed (step):
-    facilities = connector.get_json(".json")['facilities']
+    facilities = connector.get_all_facilities()['facilities']
     names = map(lambda facility: facility['name'], facilities)
     world.facility = facilities[names.index(world.name)]
-    assert world.name == world.facility['name'] 
+    assert world.name == world.facility['name']
 
 @step(u'Then I should be able to read it')
 def then_i_should_be_able_to_read_it (step):
-    facility = connector.get_json(".json", world.facility['url'])
+    facility = connector.get_json(".json", world.facility['href'])
     assert world.facility['name'] == facility['name']
 
 @step(u'And I should be able to update it')
 def and_i_should_be_able_to_update_it (step):
     name = new_name()
     world.facility['name'] = name
-    connector.write(world.facility['url'], world.facility, "PUT")
-    facility = connector.get_json(".json", world.facility['url'])
+    connector.write(world.facility['href'], world.facility, "PUT")
+    facility = connector.get_json(".json", world.facility['href'])
     assert world.facility['name'] == facility['name']
 
 @step(u'Given I try to create an invalid facility')
 def given_i_try_to_create_an_invalid_facility(step):
   try:
-    connector.write(credentials['url'], {'name':"Invalid facility"})
+    connector.write(credentials['url'] + "/facilities", {'name':"i"})
   except HTTPError as error:
     world.lastResponse = error.code
 
 @step(u'Then an error is returned')
 def then_an_error_is_returned(step):
-  assert world.lastResponse == 422 
+  assert world.lastResponse == 422
--- fred_consumer/features/api.feature
+++ fred_consumer/features/api.feature
@@ -1,16 +1,16 @@
-Feature: Fred API 
+Feature: Fred API
 
   Scenario: Error
     Given I try to create an invalid facility
     Then an error is returned
 
   Scenario: Create
-    Given I have created a facility 
-    Then it should appear at the top of the feed 
-    Then I should be able to read it 
+    Given I have created a facility
+    Then it should appear at the top of the feed
+    Then I should be able to read it
 
-  Scenario: Update 
-    Given I have created a facility 
-    Then it should appear at the top of the feed 
-    And I should be able to update it 
-    Then I should be able to read it 
+  Scenario: Update
+    Given I have created a facility
+    Then it should appear at the top of the feed
+    And I should be able to update it
+    Then I should be able to read it
